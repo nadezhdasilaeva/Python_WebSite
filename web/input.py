97f4ba1829274648f19ac76bb5ec436255ae54d6
nadezhdasilaeva <147171588+nadezhdasilaeva@users.noreply.github.com>
2024-12-27 09:34:17 +0300

    Update input.py

--- web/input.py
+++ web/input.py
@@ -1,23 +1,37 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
-from sqlmodel import Session
+from starlette.responses import RedirectResponse
+from sqlmodel import Session, select
+from models import User, Avatar
 from db import get_session
+from jwt import decode
+from config import SECRET_KEY, ALGORITHM
+import base64
 
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
 
 
-
 @router.get("/")
 def input(request: Request):
     return templates.TemplateResponse("mainpage.html", {"request": request})
 
-# @router.get("/register")
-# def register(request: Request):
-#     return templates.TemplateResponse("register.html", {"request": request})
-
 
 @router.get("/home")
-def home(request: Request):
-    return templates.TemplateResponse("homepage.html", {"request": request})
+def home(request: Request, db: Session=Depends(get_session)):
+    token = request.cookies.get("access_token")
+    if not token:
+        response = RedirectResponse(url="/", status_code=302)
+        return response
+    else:
+        scheme, _, param = token.partition(" ")
+        payload = decode(param, SECRET_KEY, algorithms=[ALGORITHM])
+        id = payload.get("sub")
+        user = db.query(User).filter(User.id == id).first()
+        image_db = db.exec(select(Avatar).where(Avatar.user_id == user.id)).first()
+        if image_db:
+            image_decode = base64.b64encode(image_db.image).decode("utf-8")
+            return templates.TemplateResponse("homepage.html", {"request": request, "img": image_decode})
+        else:
+            return templates.TemplateResponse("homepage.html", {"request": request})
